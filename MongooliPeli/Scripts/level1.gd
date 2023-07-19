9a4708f6219f495bc37a193b72958ed9a2b92a09
Mac-HotDog <68908091+Mac-HotDog@users.noreply.github.com>
2023-07-19 17:54:31 +0300

    parenltu pathing

--- MongooliPeli/Scripts/level1.gd
+++ MongooliPeli/Scripts/level1.gd
@@ -115,10 +115,10 @@ func _physics_process(delta):
 		# Check if the mouse is near any edge and emit the signal
 
 		if distance_left < EDGE_THRESHOLD:
-			_on_mouse_near_edge("left")
-		if distance_right < EDGE_THRESHOLD:
-			_on_mouse_near_edge("right")
-		if distance_top < EDGE_THRESHOLD:
-			_on_mouse_near_edge("top")
-		if distance_bottom < EDGE_THRESHOLD:
 			_on_mouse_near_edge("bottom")
+		if distance_right < EDGE_THRESHOLD:
+			_on_mouse_near_edge("top")
+		if distance_top < EDGE_THRESHOLD:
+			_on_mouse_near_edge("left")
+		if distance_bottom < EDGE_THRESHOLD:
+			_on_mouse_near_edge("right")
